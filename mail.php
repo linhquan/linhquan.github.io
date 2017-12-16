48e631f51259fb1ba33647a2177e978db0a731c0
lnquan <lnquan@ucdavis.edu>
2017-12-16 03:55:10 -0800

    new update

---
+++ mail.php
@@ -0,0 +1,34 @@
+<?php
+// if the url field is empty
+if(isset($_POST['url']) && $_POST['url'] == ''){
+     // then send the form to your email
+          mail( 'linh-quan@outlook.com', 'Contact Form', print_r($_POST,true) );
+}
+// otherwise, let the spammer think that they got their message through
+
+$to = "linh-quan@outlook.com";
+$subject = "Website Form";
+$headers = 'From: '.$email."\r\n".
+'Reply-To: '.$email."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+
+$name = $_POST['name']; // required
+$email = $_POST['email']; // required
+$response = $_POST['response']; // required
+
+function clean_string($string) {
+      $bad = array("content-type","bcc:","to:","cc:","href");
+      return str_replace($bad,"",$string);
+    }
+
+
+$txt .= "Name: ".clean_string($name)."\n";
+$txt .= "Email: ".clean_string($email)."\n";
+$txt .= "Message: ".clean_string($response)."\n";
+
+
+mail($to,$subject,$txt,$headers);
+/* Redirect visitor to the thank you page */
+header('Location: thanks.html');
+
+?>
